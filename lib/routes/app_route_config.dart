64c83b1d71fe6f4c3361fc78ef857b7513b614fe
Yugesh Poudel <yugman45d@gmail.com>
2024-05-13 19:35:08 +0545

    Shared Preferences Bug resolved

--- lib/routes/app_route_config.dart
+++ lib/routes/app_route_config.dart
@@ -2,20 +2,27 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:mero_weather/pages/help_screen.dart';
 import 'package:mero_weather/pages/homepage.dart';
+import 'package:mero_weather/pages/splash_screen.dart';
 import 'package:mero_weather/routes/app_route_constant.dart';
 
 class MeroAppRouter {
   GoRouter router = GoRouter(
     routes: [
       GoRoute(
-        name: MyAppRouteConstants.helpScreen,
+        name: MyAppRouteConstants.splashScreen,
         path: '/',
+        pageBuilder: (context, state) =>
+            const MaterialPage(child: SplashScreen()),
+      ),
+      GoRoute(
+        name: MyAppRouteConstants.helpScreen,
+        path: '/helpScreen',
         pageBuilder: (context, state) =>
             const MaterialPage(child: HelpScreen()),
       ),
       GoRoute(
         name: MyAppRouteConstants.homePage,
-        path: '/home',
+        path: '/homePage',
         pageBuilder: (context, state) => const MaterialPage(child: HomePage()),
       ),
     ],
--- lib/routes/app_route_constant.dart
+++ lib/routes/app_route_constant.dart
@@ -1,4 +1,5 @@
 class MyAppRouteConstants {
+  static const String splashScreen = 'splashScreen';
   static const String helpScreen = 'helpScreen';
   static const String homePage = 'homePage';
 }
